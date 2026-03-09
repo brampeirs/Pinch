84ee3c0e92790b010c52dda7b07cf8280aebf865
brampeirs <bram.peirs@showpad.com>
2026-03-09 21:47:00 +0100

    fix: Include environment files for Vercel build

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://uqnfiqcpmsglspaffync.supabase.co',
+    anonKey: 'sb_publishable_oPeyi2v7fBKXnZxVKNEWIA_g-TyYWWu',
+  },
+};
